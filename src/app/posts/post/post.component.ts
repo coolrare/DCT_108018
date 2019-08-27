c538a5a54d9aa7f61018ea08e467ae3812f48208
Mike Huang <wellwind@mail2000.com.tw>
2019-08-27 15:05:15 +0800

    練習 async pipe 與 rxjs operators

--- src/app/posts/post/post.component.ts
+++ src/app/posts/post/post.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PostService } from 'src/app/post.service';
 import { Article } from 'src/app/articles';
+import { Observable } from 'rxjs';
+import { switchMap, map, shareReplay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-post',
@@ -9,17 +11,25 @@ import { Article } from 'src/app/articles';
   styleUrls: ['./post.component.css']
 })
 export class PostComponent implements OnInit {
-
-  article: Article;
+  // article: Article;
+  article$: Observable<Article>;
 
   constructor(private route: ActivatedRoute, private postService: PostService) {}
 
   ngOnInit() {
-    this.route.paramMap.subscribe(paramMap => {
-      const slug = paramMap.get('slug');
-      this.postService.getArticle(slug).subscribe(article => {
-        this.article = article.article;
-      });
-    });
+    // this.route.paramMap.subscribe(paramMap => {
+    //   const slug = paramMap.get('slug');
+    //   this.postService.getArticle(slug).subscribe(article => {
+    //     this.article = article.article;
+    //   });
+    // });
+
+    this.article$ = this.route.paramMap.pipe(
+      switchMap(paramMap => this.postService.getArticle(paramMap.get('slug'))),
+      map(data => data.article),
+      shareReplay(1)
+      // map(paramMap => this.postService.getArticle(paramMap.get('slug')))
+    );
+    // .subscribe(data => {});
   }
 }
--- src/app/posts/posts/posts.component.ts
+++ src/app/posts/posts/posts.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { PostService } from 'src/app/post.service';
-import { Articles } from 'src/app/articles';
+import { Articles, Article } from 'src/app/articles';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-posts',
@@ -8,15 +10,25 @@ import { Articles } from 'src/app/articles';
   styleUrls: ['./posts.component.css']
 })
 export class PostsComponent implements OnInit {
-
   articles: Articles;
+  // articles$: Observable<Articles>;
+  articles$: Observable<Article[]>;
 
-  constructor(private postService: PostService) { }
+  constructor(private postService: PostService) {}
 
   ngOnInit() {
-    this.postService.getArticles().subscribe(data => {
-      this.articles = data;
-    });
+    // this.postService.getArticles().subscribe(data => {
+    //   this.articles = data;
+    // });
+    // this.articles$ = this.postService.getArticles().pipe(
+    //   map(data => {
+    //     // .......
+    //     //
+    //     return data.articles;
+    //   })
+    // );
+    this.articles$ = this.postService.getArticles().pipe(
+      map(data => data.articles)
+    );
   }
-
 }
